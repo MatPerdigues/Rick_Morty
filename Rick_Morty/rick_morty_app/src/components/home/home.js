f4df24a8c4b8f37cb1159f6b26c71f32241ed8f6
MatPerdigues <you@example.com>
2023-01-02 22:13:10 -0300

    V1.1

---
+++ Rick_Morty/rick_morty_app/src/components/home/home.js
@@ -0,0 +1,12 @@
+import './home.css'
+import {Fragment} from 'react'
+
+export default function Home (){
+    return (
+        <Fragment>
+            <body>
+                <h1>Hola soy la Home :)</h1>
+            </body>
+        </Fragment>
+    )
+}
